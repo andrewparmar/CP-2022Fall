167bef580671921710277271adb6a226db59fe05
Andrew Parmar <andrew.parmar@gmail.com>
2022-09-06 21:52:16 -0700

    Report and zip script.

---
+++ A1-Camera_Obscura/make_zip.sh
@@ -0,0 +1,10 @@
+rm resources.zip
+zip resources.zip -j \
+    images_captured/original_image_*.png \
+    images_captured/scene.png \
+    images_setup/setup.png \
+    images_setup/pinhole.png \
+    images_setup/box_camera.jpg \
+    images_setup/large_pinholes.png \
+    images_setup/projection_example.jpg
+
