7b63ec4376fab0ae174eed9477365ee6c46ea9c5
Andrew Parmar <andrew.parmar@gmail.com>
2022-09-06 22:24:56 -0700

    Renamed extra setup images to setup_* format for submission.

--- A1-Camera_Obscura/make_zip.sh
+++ A1-Camera_Obscura/make_zip.sh
@@ -2,9 +2,6 @@ rm resources.zip
 zip resources.zip -j \
     images_captured/original_image_*.png \
     images_captured/scene.png \
-    images_setup/setup.png \
-    images_setup/pinhole.png \
-    images_setup/box_camera.jpg \
-    images_setup/large_pinholes.png \
-    images_setup/projection_example.jpg
+    images_setup/setup* \
+    images_setup/pinhole.png
 
