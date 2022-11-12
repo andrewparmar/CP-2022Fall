ee0731195b4f3a3767da90b53314b1c26b72b63c
Andrew Parmar <andrew.parmar@gmail.com>
2022-11-11 20:53:47 -0800

    Forward energy working func. Needs cleanup.

--- Project_2/seam_carving.py
+++ Project_2/seam_carving.py
@@ -371,21 +371,35 @@ class BackwardSeamCarver(BaseSeamCarver):
 
 
 class ForwardSeamCarver(BaseSeamCarver):
-    def _get_lowest_cumulative_energy(self, energy_map):
+    def get_lowest_cumulative_energy(self, energy_map):
         h, w = energy_map.shape
-        img = np.zeros((h + 2, w + 2))
-        img[1:-1, 1:-1] = cv2.cvtColor(self.working_image, cv2.COLOR_BGR2GRAY).astype(np.float64)
-
+        # img = np.zeros((h + 2, w + 2))
+        # img[1:-1, 1:-1] = cv2.cvtColor(self.working_image, cv2.COLOR_BGR2GRAY).astype(np.float64)
+        img2 = cv2.copyMakeBorder(self.working_image, 1, 1, 1, 1, borderType=cv2.BORDER_WRAP)
+        img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY).astype(np.float64)
         """
         (a) CL(i,j) = |I(i, j + 1) − I(i, j − 1)| + |I(i − 1, j) − I(i, j − 1)|
         (b) CU(i,j) = |I(i, j + 1) − I(i, j − 1)|
         (c) CR(i,j) = |I(i, j + 1) − I(i, j − 1)| + |I(i − 1, j) − I(i, j + 1)|
         """
+        u = img2[0:-2, 1:-1]
+        l = img2[1:-1, 0:-2]
+        r = img2[1:-1, 2:]
 
-        # Notes: Assumption here that the outer edges should be 0's and not a mirror.
-        c_l = np.abs(img[1:-1, 2:] - img[1:-1, 0:-2]) + np.abs(img[0:-2, 1:-1] - img[1:-1, 0:-2])
-        c_u = np.abs(img[1:-1, 2:] - img[1:-1, 0:-2])
-        c_r = np.abs(img[1:-1, 2:] - img[1:-1, 0:-2]) + np.abs(img[0:-2, 1:-1] - img[1:-1, 2:])
+        # img = cv2.cvtColor(self.working_image, cv2.COLOR_BGR2GRAY).astype(np.float64)
+        # u = np.roll(img, 1, axis=0)
+        # l = np.roll(img, 1, axis=1)
+        # r = np.roll(img, -1, axis=1)
+        #
+        # assert np.all(u2 == u)
+        # assert np.all(l2 == l)
+        # assert np.all(r2 == r)
+
+        c_u = np.abs(l - r)
+        c_l = c_u + np.abs(u - l)
+        c_r = c_u + np.abs(u - r)
+
+        # assert
 
         M = np.zeros((h+1, w+2))
         # Set top row to energy map top row
@@ -394,14 +408,33 @@ class ForwardSeamCarver(BaseSeamCarver):
         for i in range(1, h+1):
             for j in range(1, w+1):
                 a = M[i - 1, j - 1] + c_l[i - 1, j - 1]
-                b = M[i - 1, j] + c_u[i - 1, j - 1]
+                b = M[i - 1, j]     + c_u[i - 1, j - 1]
                 c = M[i - 1, j + 1] + c_r[i - 1, j - 1]
-                M[i, j] = energy_map[i-1, j-1] + min(a, b, c)
+                # M[i, j] = energy_map[i-1, j-1] + min(a, b, c)
+                M[i, j] = min(a, b, c)
+            M[i, 0], M[i, -1] = M[i, -2], M[i, 1]
 
         return M[1:, 1:-1]
 
+        # energy = np.zeros((h, w))
+        # m = np.zeros((h, w))
+        # for i in range(1, h):
+        #     mU = m[i - 1]
+        #     mL = np.roll(mU, 1)
+        #     mR = np.roll(mU, -1)
+        #
+        #     mULR = np.array([mU, mL, mR])
+        #     cULR = np.array([c_u[i], c_l[i], c_r[i]])
+        #     mULR += cULR
+        #
+        #     argmins = np.argmin(mULR, axis=0)
+        #     m[i] = np.choose(argmins, mULR)
+        #     energy[i] = np.choose(argmins, cULR)
+
+        # return m
+
     # TODO: Modify or remove this. This is a reference.
-    def get_lowest_cumulative_energy(self, energy_map):
+    def _get_lowest_cumulative_energy(self, energy_map):
         img = cv2.cvtColor(self.working_image, cv2.COLOR_BGR2GRAY).astype(np.float64)
         h, w = img.shape
         energy = np.zeros((h, w))
